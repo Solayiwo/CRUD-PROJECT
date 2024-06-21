9f96656c5603af71c4a27bca1fb539ddd9256dc2
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-21 23:37:55 +0100

    Student Model

--- record_manager/models.py
+++ record_manager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+phone_regex = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$',
+    message="Phone number must be entered in the format: '+666666666'. up to 15 digits allowed."
+)
+
+
+class Student(models.Model):
+    First_name = models.CharField(max_length=50)
+    Last_name = models.CharField(max_length=50)
+    Age = models.IntegerField()
+    Email = models.EmailField()
+    Phone = models.CharField(validators=[phone_regex], max_length=15, blank=True)
+    Location = models.CharField(max_length=255)
+    Hobby = models.CharField(max_length=255)
+
+
+    def __str__(self) -> str:
+        return "{} {}".format(self.First_name, self.Last_name)
