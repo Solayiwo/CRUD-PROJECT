2b59d1cb054309ba2b383e5665f7f83de6dbb12a
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-22 12:09:05 +0100

    Render html pages

--- record_manager/views.py
+++ record_manager/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import StudentForm
+from .models import Student
 
-# Create your views here.
+
+def student_info(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/records')
+            except Exception:
+                pass
+        else:
+            form = StudentForm()
+        return render(request, 'index.html', {'form':form})
+
+def records(request):
+    students = Student.objects.all()
+    return render(request, 'records.html', {'students':students})
+
+def edit_record(request, id):
+    student = Student.objects.get(id=id)
+    return render(request, 'edit_record.html', {'student':student})
+
+def update_record(request, id):
+    student = Student.objects.get(id=id)
+    form = StudentForm(request.POST, instance=student)
+    if form.is_valid():
+        form.save()
+        return redirect('/records')
+    return render(request, 'edit_record.html', {'student':student})
+
+def del_record(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect('/records')
