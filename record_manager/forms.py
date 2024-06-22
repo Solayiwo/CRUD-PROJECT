e5ddd3ca951d9ee6573f33325a3292d0795cb87e
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-06-22 22:54:40 +0100

    Phone Pattern

--- record_manager/forms.py
+++ record_manager/forms.py
@@ -6,3 +6,14 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = "__all__"
+        widgets = {
+            'Phone': forms.TextInput(attrs={
+                'pattern': r'^\+\d{1,15}$', 
+                'title': 'Phone number must start with "+" and followed by the country code and digits.'
+                }
+            ),
+
+        }
+
+    
+       
